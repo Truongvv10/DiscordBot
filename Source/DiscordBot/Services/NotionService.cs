fca5357252f4074a5308e68103f6197b9b86eb28
Truongvv10 <van.vo@student.hogent.be>
2024-09-02 10:38:27 +0200

    Added notionServices

--- Source/DiscordBot/Services/NotionService.cs
+++ Source/DiscordBot/Services/NotionService.cs
@@ -1,10 +1,21 @@
-﻿using System;
+﻿using DiscordBot.Configuration;
+using Notion.Client;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DiscordBot.Services {
-    internal class NotionService {
+    public class NotionService {
+        private readonly NotionConfig _notionConfig;
+        private readonly NotionClient _notionClient;
+
+        public NotionService(NotionConfig config) {
+            _notionConfig = config;
+            _notionClient = NotionClientFactory.Create(new ClientOptions {
+                AuthToken = _notionConfig.NotionApiKey,
+            });
+        }
     }
 }
