a35b5c5237222f51f147ce0ff0970212271166e5
Truongvv10 <van.vo@student.hogent.be>
2024-10-26 12:47:36 +0200

    Add command logger in console

--- Source/DiscordBot/Services/RequirePermissionAttribute.cs
+++ Source/DiscordBot/Services/RequirePermissionAttribute.cs
@@ -1,27 +1,45 @@
-﻿using DiscordBot.Model.Enums;
+﻿using DiscordBot.Model;
+using DiscordBot.Model.Enums;
 using DiscordBot.Utils;
 using DSharpPlus;
+using DSharpPlus.Entities;
 using DSharpPlus.SlashCommands;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using XironiteDiscordBot.Exceptions;
+using AnsiColor = DiscordBot.Utils.AnsiColor;
 
 namespace DiscordBot.Services {
     public class RequirePermissionAttribute : SlashCheckBaseAttribute {
 
-        public CommandEnum Command;
+        #region Fields
+        private CommandEnum command;
+        #endregion
 
+        #region Constructors
         public RequirePermissionAttribute(CommandEnum command) {
-            this.Command = command;
+            this.command = command;
         }
+        #endregion
 
+        #region Properties
+        public CommandEnum Command {
+            get => command;
+            set => command = value;
+        }
+        #endregion
+
+        #region Methods
         public override async Task<bool> ExecuteChecksAsync(InteractionContext ctx) {
             var id = ctx.User.Id;
             var user = await ctx.Guild.GetMemberAsync(id);
             var permission = await CacheData.GetPermission(ctx.Guild.Id, Command);
             var roles = user.Roles;
+
+            LogCommand(user);
 
             if (permission.AllowEveryone) {
                 return true;
@@ -37,5 +55,10 @@ namespace DiscordBot.Services {
                 return false;
             }
         }
+
+        private void LogCommand(DiscordUser user) {
+            Console.WriteLine($"{AnsiColor.RESET}[{DateTime.Now}] {AnsiColor.GREEN}{user.Username} issued command: {AnsiColor.YELLOW}/{command.ToString().ToLower()}");
+        }
+        #endregion
     }
 }
