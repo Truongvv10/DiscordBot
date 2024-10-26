9824a15c71daac404bf679dec567bc09fed85f77
Truongvv10 <van.vo@student.hogent.be>
2024-10-26 12:39:25 +0200

    Forgot to commit changes

--- Source/DiscordBot/Services/DiscordService.cs
+++ Source/DiscordBot/Services/DiscordService.cs
@@ -93,17 +93,20 @@ namespace DiscordBot.Services {
 
         private async Task OnSlashCommandErrored(SlashCommandsExtension sender, SlashCommandErrorEventArgs e) {
             if (e.Exception is SlashExecutionChecksFailedException slex) {
+
+                // Loop through all failed checks
                 foreach (var check in slex.FailedChecks) {
 
                     // Check if the failed check is a RequirePermissionAttribute
                     if (check is RequirePermissionAttribute att) {
                         var embed = new DiscordEmbedBuilder()
-                            .WithAuthor("Feature doesn't work yet!", null, "https://cdn-icons-png.flaticon.com/512/2581/2581801.png")
+                            .WithAuthor($"You don't have permission for /{att.Command}", null, "https://cdn-icons-png.flaticon.com/512/2581/2581801.png")
                             .WithColor(new DiscordColor("#d82b40"));
                         await e.Context.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder().AddEmbed(embed).AsEphemeral(true));
                     }
 
                 }
+
             } else {
 
                 // Log the error
