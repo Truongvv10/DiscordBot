f65a3a10f4ad56bcc9c7400a61db1dddd5385c6e
Truongvv10 <van.vo@student.hogent.be>
2024-10-26 14:22:36 +0200

    New enum EmbedType

---
+++ Source/DiscordBot/Model/Enums/EmbedType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordBot.Model.Enums {
+    public enum EmbedType {
+        CUSTOM = 0,
+        BROADCAST = 10,
+        EVENT = 80,
+        EVENT_WINNERS = 81,
+        EVENT_REMINDER = 82,
+        CHANGELOG = 90,
+    }
+}
