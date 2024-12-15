757f53e560b95d1cf45e6072003f0b53be8fe25c
Truongvv10 <van.vo@student.hogent.be>
2024-12-15 13:52:16 +0100

    Improvements on database switching

---
+++ Source/DLL/Contexts/DataContextAbstract.cs
@@ -0,0 +1,130 @@
+﻿using BLL.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLL.Contexts {
+    public abstract class DataContextAbstract : DbContext {
+
+
+        #region Constructors
+        protected DataContextAbstract(string? connectionString = null) {
+            ConnectionString = connectionString;
+        }
+        #endregion
+
+        #region Properties
+        public virtual string ConnectionString { get; set; }
+        public DbSet<Guild> Servers { get; set; }
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<Template> Templates { get; set; }
+        public DbSet<Embed> Embeds { get; set; }
+        public DbSet<Settings> Settings { get; set; }
+        #endregion
+
+        #region Methods
+        protected override void OnModelCreating(ModelBuilder builder) {
+            // Server
+            builder.Entity<Guild>().ToTable("Guilds");
+            builder.Entity<Guild>()
+                .HasKey(s => s.GuildId);
+            builder.Entity<Guild>()
+                .Property(s => s.GuildId)
+                .ValueGeneratedNever();
+            builder.Entity<Guild>()
+                .HasMany(s => s.Messages)
+                .WithOne(m => m.guild)
+                .HasForeignKey(m => m.GuildId);
+            builder.Entity<Guild>()
+                .HasMany(s => s.Templates)
+                .WithOne()
+                .HasForeignKey(m => m.GuildId)
+                .OnDelete(DeleteBehavior.NoAction);
+            builder.Entity<Guild>()
+                .HasOne(s => s.Settings)
+                .WithOne()
+                .HasForeignKey<Settings>(s => s.GuildId);
+
+            // Message
+            builder.Entity<Message>().ToTable("Messages");
+            builder.Entity<Message>(entity => {
+                entity.HasKey(m => new { m.MessageId, m.GuildId });
+                entity.Property(e => e.MessageId).ValueGeneratedNever();
+                entity.Property(e => e.GuildId).ValueGeneratedNever();
+                entity.HasOne(m => m.guild)
+                    .WithMany(g => g.Messages)
+                    .HasForeignKey(m => m.GuildId)
+                    .OnDelete(DeleteBehavior.Cascade);
+            });
+            builder.Entity<Message>()
+                .HasIndex(m => new { m.GuildId, m.MessageId });
+            builder.Entity<Message>()
+                .Property(m => m.Data)
+                .HasConversion(
+                    v => JsonConvert.SerializeObject(v),
+                    v => JsonConvert.DeserializeObject<Dictionary<string, string>>(v))
+                .Metadata.SetValueComparer(new ValueComparer<Dictionary<string, string>>(
+                    (c1, c2) => c1.SequenceEqual(c2),
+                    c => c.Aggregate(0, (a, v) => HashCode.Combine(a, v.GetHashCode())),
+                    c => c.ToDictionary(k => k.Key, v => v.Value)));
+            builder.Entity<Message>()
+                .Property(m => m.Childs)
+                .HasConversion(
+                    v => JsonConvert.SerializeObject(v),
+                    v => JsonConvert.DeserializeObject<Dictionary<ulong, ulong>>(v))
+                .Metadata.SetValueComparer(new ValueComparer<Dictionary<ulong, ulong>>(
+                    (c1, c2) => c1.SequenceEqual(c2),
+                    c => c.Aggregate(0, (a, v) => HashCode.Combine(a, v.GetHashCode())),
+                    c => c.ToDictionary(k => k.Key, v => v.Value)));
+
+            // Embed
+            builder.Entity<Embed>().ToTable("Embeds");
+            builder.Entity<Embed>()
+                .HasKey(e => new { e.GuildId, e.MessageId });
+            builder.Entity<Embed>()
+                .HasOne(e => e.Message)
+                .WithOne(m => m.Embed)
+                .HasForeignKey<Embed>(e => new { e.GuildId, e.MessageId })
+                .OnDelete(DeleteBehavior.Cascade); ;
+            builder.Entity<Embed>()
+                .Property(e => e.Fields)
+                .HasConversion(
+                    v => JsonConvert.SerializeObject(v),
+                    v => JsonConvert.DeserializeObject<List<(string, string, bool)>>(v))
+                .Metadata.SetValueComparer(new ValueComparer<List<(string Title, string Value, bool Inline)>>(
+                    (c1, c2) => c1.SequenceEqual(c2),
+                    c => c.Aggregate(0, (a, v) => HashCode.Combine(a, v.GetHashCode())),
+                    c => c.ToList()));
+
+
+            // Template
+            builder.Entity<Template>().ToTable("Templates");
+            builder.Entity<Template>(entity => {
+                entity.HasKey(t => new { t.GuildId, t.Name });
+                entity.Property(t => t.GuildId).ValueGeneratedNever();
+                entity.Property(t => t.Name).ValueGeneratedNever();
+                entity.Property(t => t.Message).HasConversion(
+                    v => JsonConvert.SerializeObject(v),
+                    v => JsonConvert.DeserializeObject<Message>(v));
+                entity.Property(t => t.Name)
+                    .HasMaxLength(32);
+            });
+
+            // Settings
+            builder.Entity<Settings>().ToTable("Settings");
+            builder.Entity<Settings>(entity => {
+                entity.HasKey(s => s.GuildId);
+                entity.Property(s => s.GuildId).ValueGeneratedNever();
+            });
+
+            // Return
+            base.OnModelCreating(builder);
+        }
+        #endregion
+    }
+}
---
+++ Source/DLL/Repositories/DataRepository.cs
@@ -0,0 +1,312 @@
+﻿using BLL.Exceptions;
+using BLL.Interfaces;
+using BLL.Model;
+using DLL.Contexts;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLL.Repositories {
+    public class DataRepository : IDataRepository {
+        #region Fields
+        protected ICacheData cacheData;
+        protected DataContextAbstract dataContext;
+        #endregion
+
+        #region Constructors
+        protected DataRepository(ICacheData cacheData, DataContextAbstract dataContext) {
+            this.cacheData = cacheData;
+            this.dataContext = dataContext;
+        }
+        #endregion
+
+        #region Methods
+        public async Task<Message> AddMessageAsync(Message message) {
+            if (message == null)
+                throw new ServiceException($"Message can not be null.");
+            if (message.GuildId == 0)
+                throw new ServiceException($"Guild Id can not be null.");
+            if (message.MessageId == 0)
+                throw new ServiceException($"Message Id can not be null.");
+            await dataContext.Messages.AddAsync(message);
+            await dataContext.SaveChangesAsync();
+            cacheData.AddMessage(message.GuildId, message);
+            return message;
+        }
+
+        public async Task AddServerAsync(ulong guildId) {
+            await dataContext.AddAsync(new Guild() { GuildId = guildId });
+            await dataContext.SaveChangesAsync();
+        }
+
+        public async Task<Settings> AddSettingsAsync(Settings settings) {
+            if (settings == null)
+                throw new ServiceException($"Settings can not be null.");
+            if (settings.GuildId == 0)
+                throw new ServiceException($"Guild Id can not be null.");
+            await dataContext.Settings.AddAsync(settings);
+            await dataContext.SaveChangesAsync();
+            cacheData.AddSettings(settings.GuildId, settings);
+            return settings;
+        }
+
+        public async Task<Template> AddTemplateAsync(Template template) {
+            await dataContext.Templates.AddAsync(template);
+            await dataContext.SaveChangesAsync();
+            return template;
+        }
+
+        public async Task<bool> AnyMessageAsync(ulong guildId, ulong messageId) {
+            return await dataContext.Messages
+                .AsNoTracking()
+                .AnyAsync(m => m.GuildId == guildId && m.MessageId == messageId);
+        }
+
+        public async Task<bool> AnyServerAsync(ulong guildId) {
+            return await dataContext.Servers
+                .AsNoTracking()
+                .AnyAsync(s => s.GuildId == guildId);
+        }
+
+        public async Task<bool> AnySettingsAsync(ulong guildId) {
+            return await dataContext.Settings
+                .AsNoTracking()
+                .AnyAsync(s => s.GuildId == guildId);
+        }
+
+        public async Task<bool> AnyTemplateAsync(ulong guildId, string name) {
+            return await dataContext.Templates
+                .AsNoTracking()
+                .AnyAsync(t => t.GuildId == guildId && t.Name == name);
+        }
+
+        public async Task CtxSaveAndClear() {
+            await dataContext.SaveChangesAsync();
+            dataContext.ChangeTracker.Clear();
+        }
+
+        public async Task<List<Guild>> GetAllServersAsync() {
+            return await dataContext.Servers
+                .ToListAsync();
+        }
+
+        public async Task<List<Template>> GetAllTemplatesAsync(ulong guildId) {
+            return await dataContext.Templates
+                .Where(t => t.GuildId == guildId)
+                .AsNoTracking()
+                .ToListAsync();
+        }
+
+        public async Task<Message?> GetMessageAsync(ulong guildId, ulong messageId) {
+            if (cacheData.Messages.TryGetValue((guildId, messageId), out var message)) {
+                return message;
+            } else {
+                var result = await dataContext.Messages
+                    .Where(m => m.GuildId == guildId && m.MessageId == messageId)
+                    .Include(m => m.Embed)
+                    .AsNoTracking()
+                    .FirstOrDefaultAsync();
+                if (result != null) cacheData.AddMessage(guildId, result);
+                return result;
+            }
+        }
+
+        public async Task<Guild?> GetServerAsync(ulong guildId, bool includeMessage = true) {
+            IQueryable<Guild> query = dataContext.Servers.Where(s => s.GuildId == guildId);
+            if (includeMessage) query = query.Include(m => m.Messages).ThenInclude(m => m.Embed);
+            return await query
+                .AsNoTracking()
+                .FirstOrDefaultAsync();
+
+        }
+
+        public async Task<Settings?> GetSettingsAsync(ulong guildId) {
+            return await dataContext.Settings
+                .Where(s => s.GuildId == guildId)
+                .AsNoTracking()
+                .FirstOrDefaultAsync();
+        }
+
+        public async Task<Template?> GetTemplateAsync(ulong guildId, string name) {
+            var template = await dataContext.Templates
+                .Where(t => t.GuildId == guildId && t.Name == name)
+                .AsNoTracking()
+                .FirstOrDefaultAsync();
+            if (template == null) {
+                return cacheData.GetTemplate(name);
+            } else {
+                return new Template(template.GuildId, template.Name, template.Message.DeepClone());
+            }
+        }
+
+        public async Task LoadGuildsAsync(IEnumerable<ulong> guildIds) {
+            foreach (var id in guildIds) {
+                if (!await AnyServerAsync(id)) {
+                    await AddServerAsync(id);
+                }
+            }
+        }
+
+        public Task LoadSettingsFromGuildsAsync() {
+            throw new NotImplementedException();
+        }
+
+        public async Task LoadTemplatesAsync() {
+            await cacheData.LoadTemplates();
+        }
+
+        public async Task LoadTimeZonesAsync() {
+            await Task.Run(() => cacheData.LoadTimeZones());
+        }
+
+        public async Task RemoveMessageAsync(Message message) {
+            var tracked = dataContext.Messages.Local.FirstOrDefault(m => m.MessageId == message.MessageId && m.GuildId == m.GuildId);
+            if (tracked != null) dataContext.Entry(tracked).State = EntityState.Detached;
+            dataContext.Messages.Remove(message);
+            await CtxSaveAndClear();
+        }
+
+        public async Task RemoveServerAsync(Guild guild) {
+            dataContext.Servers.Remove(guild);
+            await CtxSaveAndClear();
+        }
+
+        public async Task RemoveSettingsAsync(Settings settings) {
+            dataContext.Settings.Remove(settings);
+            await CtxSaveAndClear();
+        }
+
+        public async Task RemoveTemplateAsync(Template template) {
+            dataContext.Templates.Remove(template);
+            await CtxSaveAndClear();
+        }
+
+        public async Task<Message> UpdateMessageAsync(Message message) {
+            if (message == null)
+                throw new ServiceException($"Message can not be null.");
+            if (message.GuildId == 0)
+                throw new ServiceException($"Guild Id can not be null.");
+            if (message.MessageId == 0)
+                throw new ServiceException($"Message Id can not be null.");
+
+            await RemoveMessageAsync(message);
+            cacheData.DeleteMessage(message.GuildId, message.MessageId);
+            await AddMessageAsync(message);
+            return message;
+        }
+
+        public async Task<Message> UpdateMessageAsync(Message message, string component) {
+            if (message == null)
+                throw new ServiceException($"Message can not be null.");
+            if (message.GuildId == 0)
+                throw new ServiceException($"Guild Id can not be null.");
+            if (message.MessageId == 0)
+                throw new ServiceException($"Message Id can not be null.");
+
+            if (cacheData.AnyMessage(message.GuildId, message.MessageId))
+                cacheData.UpdateMessage(message.GuildId, message);
+            else cacheData.AddMessage(message.GuildId, message);
+
+            var fetched = await GetMessageAsync(message.GuildId, message.MessageId);
+            if (fetched != null) {
+                switch (component) {
+
+                    case Identity.SELECTION_TITLE:
+                        fetched.Embed.Title = message.Embed.Title;
+                        fetched.Embed.TitleUrl = message.Embed.TitleUrl;
+                        dataContext.Entry(fetched.Embed).Property(e => e.Title).IsModified = true;
+                        dataContext.Entry(fetched.Embed).Property(e => e.TitleUrl).IsModified = true;
+                        break;
+
+                    case Identity.SELECTION_DESCRIPTION:
+                        fetched.Embed.Description = message.Embed.Description;
+                        dataContext.Entry(fetched.Embed).Property(e => e.Description).IsModified = true;
+                        break;
+
+                    case Identity.SELECTION_CONTENT:
+                        fetched.Content = message.Content;
+                        dataContext.Entry(fetched).Property(m => m.Content).IsModified = true;
+                        break;
+
+                    case Identity.SELECTION_FOOTER:
+                        fetched.Embed.Footer = message.Embed.Footer;
+                        fetched.Embed.FooterUrl = message.Embed.FooterUrl;
+                        dataContext.Entry(fetched.Embed).Property(e => e.Footer).IsModified = true;
+                        dataContext.Entry(fetched.Embed).Property(e => e.FooterUrl).IsModified = true;
+                        break;
+
+                    case Identity.SELECTION_AUTHOR:
+                        fetched.Embed.Author = message.Embed.Author;
+                        fetched.Embed.AuthorLink = message.Embed.AuthorLink;
+                        fetched.Embed.AuthorUrl = message.Embed.AuthorUrl;
+                        dataContext.Entry(fetched.Embed).Property(e => e.Author).IsModified = true;
+                        dataContext.Entry(fetched.Embed).Property(e => e.AuthorLink).IsModified = true;
+                        dataContext.Entry(fetched.Embed).Property(e => e.AuthorUrl).IsModified = true;
+                        break;
+
+                    case Identity.SELECTION_COLOR:
+                        fetched.Embed.Color = message.Embed.Color;
+                        dataContext.Entry(fetched.Embed).Property(e => e.Color).IsModified = true;
+                        break;
+
+                    case Identity.SELECTION_IMAGE:
+                        fetched.Embed.Image = message.Embed.Image;
+                        dataContext.Entry(fetched.Embed).Property(e => e.Image).IsModified = true;
+                        break;
+
+                    case Identity.SELECTION_THUMBNAIL:
+                        fetched.Embed.Thumbnail = message.Embed.Thumbnail;
+                        dataContext.Entry(fetched.Embed).Property(e => e.Thumbnail).IsModified = true;
+                        break;
+
+                    case Identity.SELECTION_PINGROLE:
+                        fetched.Roles = message.Roles;
+                        dataContext.Entry(fetched).Property(e => e.Roles).IsModified = true;
+                        break;
+
+                    case Identity.SELECTION_FIELD_ADD:
+                        fetched.Embed.SetFields(message.Embed.Fields.ToList());
+                        dataContext.Entry(fetched.Embed).Property(e => e.Fields).IsModified = true;
+                        break;
+
+                    case Identity.SELECTION_FIELD_REMOVE:
+                        fetched.Embed.SetFields(message.Embed.Fields.ToList());
+                        dataContext.Entry(fetched.Embed).Property(e => e.Fields).IsModified = true;
+                        break;
+
+                    case Identity.SELECTION_PLACEHOLDER:
+                        fetched.Data = message.Data;
+                        dataContext.Entry(fetched).Property(m => m.Data).IsModified = true;
+                        break;
+
+                    default:
+                        return message;
+                }
+                await CtxSaveAndClear();
+                return fetched;
+            } else throw new ServiceException($"Message with id \"{message.MessageId}\" in guild \"{message.GuildId}\" doesn't exists in database.");
+        }
+
+        public async Task<Settings> UpdateSettingsAsync(Settings settings) {
+            if (settings == null)
+                throw new ServiceException($"Settings can not be null.");
+            if (settings.GuildId == 0)
+                throw new ServiceException($"Guild Id can not be null.");
+            var existingSettings = await GetSettingsAsync(settings.GuildId);
+            existingSettings = settings;
+            await dataContext.SaveChangesAsync();
+            return existingSettings;
+        }
+
+        public async Task<Template> UpdateTemplateAsync(Template template) {
+            var existingTemplate = await GetTemplateAsync(template.GuildId, template.Name);
+            existingTemplate = template;
+            await dataContext.SaveChangesAsync();
+            return existingTemplate;
+        }
+        #endregion
+    }
+}
