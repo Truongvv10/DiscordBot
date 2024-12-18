c7490834c203eaf19998291ea8782cbd835ffb81
Truongvv10 <van.vo@student.hogent.be>
2024-12-18 14:45:34 +0100

    Added countryutil to check if country exists

---
+++ Source/DiscordBot/Utils/CountryUtil.cs
@@ -0,0 +1,16 @@
+﻿using ISO3166;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APP.Utils {
+    public static class CountryUtil {
+
+        public static bool IsValidCountry(string country) {
+            return Country.List.Any(c => c.Name.Equals(country, StringComparison.OrdinalIgnoreCase));
+        }
+
+    }
+}
